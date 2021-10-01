08d3c15b4a22866e891adf3bf5fe39b27a412081
Andrew Haight <haight.haight@gmail.com>
2021-09-30 18:15:08 -0700

    code for /models/index.js

--- models/index.js
+++ models/index.js
@@ -3,14 +3,33 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { hasMany } = require('./Product');
 
 // Products belongsTo Category
+Products.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+    
+});
 
 // Categories have many Products
+Categories.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: 'product_id',
+  });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Prpduct, {
+  through: ProductTag,
+  // as: 'product_tags',
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
