bf4e11ea31a97e007cfcb46411331e89a5ddca42
Andrew Haight <haight.haight@gmail.com>
2021-09-30 18:52:53 -0700

    init tag-routes code/ needs put/delete

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,17 +4,41 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+  try {
+    const productTagData = await Tag.findAll({
+      include: [{model: Product }],
+    });
+    res.status(200).json(pTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if (!tagData) {
+      res.status(404).json({message: "Ah ah ah, tag not found..."});
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
-  // create a new tag
+  try {
+    const tagData = await Tag.create({
+      tag_id: req.body.tag_id,
+    })
+    res.status(200).json(newTag);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
